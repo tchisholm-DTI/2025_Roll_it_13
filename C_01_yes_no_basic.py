6b620539a4e4a99071d861604ea37d72e068eb97
tchisholm-DTI <68577478+tchisholm-DTI@users.noreply.github.com>
2024-12-06 10:07:56 +1300

    Looping yes/no checker

--- C_01_yes_no_basic.py
+++ C_01_yes_no_basic.py
@@ -1,9 +1,18 @@
-want_instructions = input("Do you want to see the instructions?").lower()
 
-# Check the user says yes/no
-if want_instructions == "yes" or want_instructions == "y":
-    print("You said yes.")
-elif want_instructions == "no" or want_instructions == "n":
-    print("You said no.")
-else:
-    print("Please enter yes/no.")
+
+while True:
+
+    want_instructions = input("Do you want to see the instructions?").lower()
+    # Check the user says yes/no
+    if want_instructions == "yes" or want_instructions == "y":
+        print("You said yes.")
+        break
+    elif want_instructions == "no" or want_instructions == "n":
+        print("You said no.")
+        break
+    else:
+        print("Please enter yes/no.")
+        continue
+
+print("We are done")
+
