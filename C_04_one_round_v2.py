44e5d72aacd061f35a59e6e9ef168a161138e6a5
tchisholm-DTI <68577478+tchisholm-DTI@users.noreply.github.com>
2024-12-06 15:03:15 +1300

    Untested single rounds component

--- C_04_one_round_v2.py
+++ C_04_one_round_v2.py
@@ -18,12 +18,10 @@ def initial_points(which_player):
     return total, double
 
 
+# Main starts here
 # Roll the dice for the user and note of they got a double
 initial_user = initial_points("User")
 initial_comp = initial_points("Comp")
-
-print("Initial User", initial_user)
-print("Initial Computer", initial_comp)
 
 # Retrieve user points (first item returned from function)
 user_points = initial_user[0]
@@ -35,3 +33,47 @@ double_user = initial_user[1]
 # Let the user know if they qualify for double points
 if double_user == "yes":
     print("Great news - if you win, you will earn double points!")
+
+# Assume user goes first ...
+first = "User"
+second = "Computer"
+player_1_points = user_points
+player_2_points = comp_points
+
+# If the user has fewer points, they start the game
+if user_points < comp_points:
+    print("You start because your initial roll was less than the computer\n")
+
+# If the user and computer roll equal points, the user is player 1
+elif user_points == comp_points:
+    print("The initial rolls were the same, the user starts")
+
+# If the computer has fewer points, switch the computer to 'player 1'
+else:
+    player_1_points, player_2_points = player_2_points, player_1_points
+    first, second = second, first
+
+# Loop until we have a winner
+while player_1_points < 13 and player_2_points < 13:
+    print()
+    input("Press <enter> to continue this round\n")
+
+    # First person rolls the die and score is updated
+    player_1_roll = random.randint(1, 6)
+    player_1_points += player_1_roll
+
+    print(f"{first}: Rolled a {player_1_roll} - has {player_1_points} points")
+
+    # If the first person's score is over 13, end the round
+    if player_1_points >= 13:
+        break
+
+    # Second person rolls the die and score is updated
+    player_2_roll = random.randint(1, 6)
+    player_2_points += player_2_roll
+
+    print(f"{second}: Rolled a {player_2_roll} - has {player_2_points} points")
+
+    print(f"{first}: {player_1_points} | {second}: {player_2_points}")
+
+print("End of round")
